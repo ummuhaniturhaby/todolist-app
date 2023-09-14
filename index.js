365d09d5e7acc20dc44502dbc2fded9e69de72ac
ummuhaniturhaby <ummuhaniturhaby@gmail.com>
2023-09-14 20:58:32 +0700

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+const formTask = document.querySelector(".form-task");
+
+formTask.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputForm = document.querySelector(".input-form");
+
+
+    const wrapperList = document.querySelector(".task-list-wrapper");
+
+    const taskList = document.createElement("li");
+
+    taskList.innerHTML = inputForm.value;
+
+    wrapperList.append(taskList);
+});
